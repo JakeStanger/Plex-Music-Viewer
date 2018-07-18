61ac3a685d4245a1344d8dd08c55d0e8b1448707
Jake Stanger <jakestanger@gmail.com>
2018-07-18 19:44:43 +0100

    Create defaults module. Will be responsible for setting the any default settings if they are missing and writing them to file.

---
+++ defaults.py
@@ -0,0 +1,23 @@
+default_settings = {
+    "librarySection": "Music",
+    "serverAddress": "http://localhost:32400",
+    "interfaceToken": "",
+    "searchResults": {
+        "artistResults": 10,
+        "albumResults": 10,
+        "trackResults": 20
+    },
+    "serverToken": "",
+    "database": {
+        "user": "",
+        "password": "",
+        "database": "",  # TODO Automatically create database and tables if missing
+        "hostname": "localhost"
+    },
+    "secret_key": "",
+    "genius_api": ""
+}
+
+
+def set_defaults():
+    pass
