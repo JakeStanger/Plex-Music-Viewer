06c13d0faa6d0574f6133d1ea1e072696654746d
Jake Stanger <jakestanger@gmail.com>
2018-08-24 15:11:52 +0100

    Put table create queries into dictionary

--- defaults.py
+++ defaults.py
@@ -27,90 +27,89 @@ default_settings = {
     "newUserPerms": [0, 0, 0]
 }
 
-user_table = """create table users
-(
-  user_id     bigint auto_increment
-    primary key,
-  username    text                   not null,
-  password    text                   not null,
-  music_perms tinyint(6) default '3' not null,
-  movie_perms tinyint(6) default '3' not null,
-  tv_perms    tinyint(6) default '3' not null,
-  is_admin    tinyint(1) default '0' not null,
-  is_deleted  tinyint(1) default '0' not null
-);
+default_tables = {
+    'users': """
+        create table users
+        (
+          user_id     bigint auto_increment
+            primary key,
+          username    text                   not null,
+          password    text                   not null,
+          music_perms tinyint(6) default '3' not null,
+          movie_perms tinyint(6) default '3' not null,
+          tv_perms    tinyint(6) default '3' not null,
+          is_admin    tinyint(1) default '0' not null,
+          is_deleted  tinyint(1) default '0' not null
+        );
+""",
+    'artists': """
+        create table artists
+        (
+          library_key int    not null,
+          name        text   not null,
+          name_sort   text   not null,
+          thumb       bigint null,
+          album_count int    not null,
+          constraint artists_library_key_uindex
+          unique (library_key)
+        );
+        
+        alter table artists
+          add primary key (library_key);
+""",
+    'albums': """
+        create table albums
+        (
+          library_key int         not null,
+          name        text        not null,
+          name_sort   text        not null,
+          artist_key  int         not null,
+          artist_name text        not null,
+          year        smallint(6) not null,
+          genres      text        null,
+          thumb       bigint      null,
+          track_count smallint(6) not null,
+          total_size  bigint      not null,
+          constraint albums_library_key_uindex
+          unique (library_key),
+          constraint albums_artists_library_key_fk
+          foreign key (artist_key) references artists (library_key)
+            on update cascade
+            on delete cascade
+        );
+            
+            alter table albums
+              add primary key (library_key);
+            """,
+    'tracks': """
+        create table tracks
+        (
+          library_key  int auto_increment,
+          name         text      not null,
+          name_sort    text      not null,
+          artist_key   int       not null,
+          artist_name  text      not null,
+          album_key    int       not null,
+          album_name   text      not null,
+          duration     int       not null,
+          track_num    tinyint   not null,
+          disc_num     tinyint   not null,
+          download_url text      not null,
+          bitrate      mediumint not null,
+          size         bigint    not null,
+          format       tinytext  not null,
+          constraint tracks_library_key_uindex
+          unique (library_key),
+          constraint tracks_albums_library_key_fk
+          foreign key (album_key) references albums (library_key),
+          constraint tracks_artists_library_key_fk
+          foreign key (artist_key) references artists (library_key)
+        );
+        
+        alter table tracks
+          add primary key (library_key);
 """
-
-
-artist_table = """
-create table artists
-(
-  library_key int    not null,
-  name        text   not null,
-  name_sort   text   not null,
-  thumb       bigint null,
-  album_count int    not null,
-  constraint artists_library_key_uindex
-  unique (library_key)
-);
-
-alter table artists
-  add primary key (library_key);
-"""
-
-album_table = """create table albums
-(
-  library_key int         not null,
-  name        text        not null,
-  name_sort   text        not null,
-  artist_key  int         not null,
-  artist_name text        not null,
-  year        smallint(6) not null,
-  genres      text        null,
-  thumb       bigint      null,
-  track_count smallint(6) not null,
-  total_size  bigint      not null,
-  constraint albums_library_key_uindex
-  unique (library_key),
-  constraint albums_artists_library_key_fk
-  foreign key (artist_key) references artists (library_key)
-    on update cascade
-    on delete cascade
-);
-
-alter table albums
-  add primary key (library_key);
-"""
-
-
-track_table = """
-create table tracks
-(
-  library_key  int auto_increment,
-  name         text      not null,
-  name_sort    text      not null,
-  artist_key   int       not null,
-  artist_name  text      not null,
-  album_key    int       not null,
-  album_name   text      not null,
-  duration     int       not null,
-  track_num    tinyint   not null,
-  disc_num     tinyint   not null,
-  download_url text      not null,
-  bitrate      mediumint not null,
-  size         bigint    not null,
-  format       tinytext  not null,
-  constraint tracks_library_key_uindex
-  unique (library_key),
-  constraint tracks_albums_library_key_fk
-  foreign key (album_key) references albums (library_key),
-  constraint tracks_artists_library_key_fk
-  foreign key (artist_key) references artists (library_key)
-);
-
-alter table tracks
-  add primary key (library_key);
-"""
+}
 
 
 def set_missing_as_default(settings: dict):
@@ -123,4 +122,4 @@ def set_missing_as_default(settings: dict):
 
 def write_settings(settings: dict):
     with open('settings.json', 'w') as f:
-        f.write(dumps(settings,indent=2))
+        f.write(dumps(settings, indent=2))
