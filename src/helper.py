316554eec437afbd191b0ee6f8eb4d460b6565e4
Jake Stanger <jakestanger@gmail.com>
2018-10-29 22:22:56 +0000

    Add size and duration formation functions

--- src/helper.py
+++ src/helper.py
@@ -1,4 +1,6 @@
-from flask import abort, Response, request
+import math
+
+from flask import abort, Response
 
 
 def generate_secret_key(length=64):
@@ -9,10 +11,25 @@ def generate_secret_key(length=64):
     return b64encode(random_bytes).decode('utf-8')
 
 
-def throw_error(num, error):
+def throw_error(num: int, error):
     """
     Throws an HTTP error
     :param num: The HTTP error number
     :param error: An error message
     """
     abort(Response(error, num))
+
+
+def format_size(size: int) -> str:
+    sizes = ['B', 'kiB', 'MiB', 'GiB', 'TiB']
+    if size == 0:
+        return '0B'
+
+    i = int(math.floor(math.log(size) / math.log(1024)))
+    return ('%.3g' % (size / math.pow(1024, i))) + ' ' + sizes[i]
+
+
+def format_duration(duration: int) -> str:
+    minutes = math.floor(duration / 60000)
+    seconds = math.floor((duration % 60000) / 1000)
+    return str(minutes) + ":" + ('0' if seconds < 10 else '') + str(seconds)
