724909f7bc3e0f7b55eda88a9200196641b23b4e
Jake Stanger <jakestanger@gmail.com>
2018-11-16 23:31:38 +0000

    Make big again!

--- src/database/models.py
+++ src/database/models.py
@@ -142,7 +142,7 @@ class Track(db.Model):
     download_url = db.Column(db.Text)
     bitrate = db.Column(db.Integer)
     size = db.Column(db.BigInteger)
-    format = db.Column(db.String(10))
+    format = db.Column(db.String(32))
 
     plex_id = db.Column(db.BigInteger, unique=True)
 
